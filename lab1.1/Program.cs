49589f43f2b39f2ada17f169f058a7c10cf889b3
IAVE12 <119600641+IAVE12@users.noreply.github.com>
2023-02-20 08:16:31 +0300

    Add files via upload

---
+++ lab1.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+                Console.WriteLine(line);
+
+            }
+        }
+    }
+}
